05ebc16b60af8344071afd808b8dda2cc9f5d6ed
Pheak Rattana <rattana.pheak.rp@gmail.com>
2023-02-18 00:41:08 +0100

    Update From Main Group Repository

--- index.php
+++ index.php
@@ -2,10 +2,7 @@
 session_start();
 
 
-
-
 require_once __DIR__ . './src/controllers/homePageCont.php';
-
 
 
 if (!empty($_GET) && isset($_GET)) {
@@ -16,24 +13,6 @@ if (!empty($_GET) && isset($_GET)) {
         require_once __DIR__ . './src/controllers/registerCont.php';
         getViewRegister();
     }
-    // } else if ( $_GET['action'] == 'askRegister'){
-    //     verifInscription ();
-    
-    // }
-//   if (!empty($_GET['action'] === 'register')) {
-//     registerView();
-//     // if($_POST){
-//       // var_dump($_POST);
-//       // var_dump($errors);
-//     // }
-//   } else if (!empty($_GET['action'] === 'login')){
-//     loginView();
-//   } else if(!empty($_GET['action'] === 'pictures')){
-//     picturesView();
-//   } else if ($_GET['action'] === 'videos'){
-//     videosView();
-//   }
-
 
 } else {
   getViewHomePage();
