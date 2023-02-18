30fb764df6bbd4e9f30ac617a7286a6f5af2ed87
Pheak Rattana <rattana.pheak.rp@gmail.com>
2023-02-18 19:31:37 +0100

    Style DisconnectView
    Style DisconnectView

--- src/view/disconnectView.php
+++ src/view/disconnectView.php
@@ -2,17 +2,19 @@
 ob_start();
 $title = 'Déconnexion';
 require_once __DIR__ . './../../src/view/includes/head.php';
-
 ?>
+
+<link rel="stylesheet" href="./public/css/style.css">
+
 <body>
-  
-<div>
-  <p>Vous avez bien été déconnecter</p>
-  <p>Merci de votre visite</p>
-  <a href="./">Page d'accueil</a>
-</div>
+  <div id="disconnect">
+    <p class="text-dc">Vous avez bien été déconnecter</p>
+    <p class="text-dc">Merci de votre visite</p>
+    <a href="./" class="text-dc">Page d'accueil</a>
+  </div>
 </body>
 
 <?= $content = ob_get_clean();
 
+require_once __DIR__ . './../../src/view/includes/footer.php';
 session_destroy();
